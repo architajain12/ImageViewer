e6d55ae9af55733f81c1071358f551ac8788d343
Archita Jain <jainarchita12@gmail.com>
2021-06-26 03:12:09 +0530

    Fixed login card margin & updated profile picture

--- src/assets/UserInfo.js
+++ src/assets/UserInfo.js
@@ -1,7 +1,6 @@
 export const UserInfo = {
-    profilePicURL: "https://instagram.fdel27-4.fna.fbcdn.net/v/t51.2885-19/s150x150/206147444_100212485638459_1692161665704099367_n.jpg?tp=1&_nc_ht=instagram.fdel27-4.fna.fbcdn.net&_nc_ohc=uvkLd-31Y0IAX8NiTAc&edm=ABfd0MgBAAAA&ccb=7-4&oh=2359489b55bcd57c39dd97186c1a6b54&oe=60DD2AE0&_nc_sid=7bff83",
-    userName: "abc xyz",
+    profilePicURL: "https://instagram.fdel27-4.fna.fbcdn.net/v/t51.2885-19/s150x150/208633731_498750061415116_9030460370481497323_n.jpg?tp=1&_nc_ht=instagram.fdel27-4.fna.fbcdn.net&_nc_ohc=BDLssWyqgIIAX_hD-Ho&edm=ABfd0MgBAAAA&ccb=7-4&oh=471e0d3962772b2db7a2668bada0c6c4&oe=60DD67A9&_nc_sid=7bff83",
     following: 200,
     followers: 400,
-    totalPosts: 50
+    totalPosts: 3
 }
--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -49,7 +49,7 @@ class Login extends Component{
     render(){
         return(
             <div>
-                <Header title="Image Viewer" section="login"/>
+                <Header title="Image Viewer" section="Login"/>
                 <div className = "card-container">
                     <Card className = "login-card">
                         <CardContent>
